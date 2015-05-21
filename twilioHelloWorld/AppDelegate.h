30fa06b26e0171a03d57a1e30d478f36fd74cf24
Patrick Wilson <pdubslax@yahoo.com>
2015-05-21 10:33:39 -0700

    addhelloworld

---
+++ twilioHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twilioHelloWorld
+//
+//  Created by Patrick Wilson on 5/21/15.
+//  Copyright (c) 2015 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twilioHelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twilioHelloWorld
+//
+//  Created by Patrick Wilson on 5/21/15.
+//  Copyright (c) 2015 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
